3d300ecd85e1823e235421b327a083ed2f79d7b3
jessicaelee <57161537+jessicaelee@users.noreply.github.com>
2021-06-03 15:48:23 -0700

    Update index.js

--- src/Introduction/index.js
+++ src/Introduction/index.js
@@ -11,7 +11,7 @@ function Introduction() {
           <img className="photo" src={image} alt="jessica" />
         </div>
         <div>
-          <p>Hello! I am a software engineer based in San Francisco (I actually grew up here!). I attended Pepperdine University in Southern California where I majored in Accounting. I spent a few years as an accountant until I decided to leave that (boring) life behind and pursue my interests in software development. It's been the best decision I've made and I haven't looked back since.</p>
+          <p>Hey! I am a software engineer based in San Francisco (I actually grew up here!). I attended Pepperdine University in Southern California where I majored in Accounting. I spent a few years as an accountant until I decided to leave that (boring) life behind and pursue my interests in software development. It's been the best decision I've made and I haven't looked back since.</p>
         </div>
       </div>
     </Card>
